4920fc626812128332d999381f2b8d903bc38bf2
Rafaelvillalpando21 <rafaelvillalpando21@gmail.com>
2024-12-06 18:56:07 -0700

    Actualización en app.js

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
-const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require("@bot-whatsapp/bot");
 require("dotenv").config();
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MongoAdapter = require('@bot-whatsapp/database/mongo')
+const QRPortalWeb = require("@bot-whatsapp/portal");
+const BaileysProvider = require("@bot-whatsapp/provider/baileys");
+const MockAdapter = require("@bot-whatsapp/database/mock");
 
-const { writeToSheet, appendToSheet, readSheet, getFilteredData } = require("./googlesheets");
-
-
-const flowSheets = addKeyword("sheets").addAnswer("Este es el flujo Sheets", null,
-    async (ctx, ctxFn) => {
-        const values = [
-            ['mimi', 25, 'chile', "teamo"],
-            ['rafa', 30, 'mexico', "yomas"]
-        ];
-
-
-        //let range = 'G2';
-        //await writeToSheet(values, range); // Escribe datos en un rango específico.
-
-        //await appendToSheet(values); // Agrega datos columna por columna.
-        //range = 'A1:B10';
-        //const response = await readSheet(range); // Lee datos de un rango.
-
-        const response = await getFilteredData("A", "Mark")
-        console.log(response);
-    });
+const flowPrincipal = addKeyword(EVENTS.WELCOME)
+    .addAnswer("Saludos desde cloud", null,
+        async (ctx, ctxFn) => {
+            console.log("Estoy correctamente deployado");
+        }
+    );
 
 const main = async () => {
-    const adapterDB = new MongoAdapter({
-        dbUri: process.env.MONGO_DB_URL,
-        dbName: "test", // Cambia esto si tu base de datos tiene otro nombre
-    });
-
-    const adapterFlow = createFlow([flowSheets]);
+    const adapterDB = new MockAdapter();
+    const adapterFlow = createFlow([flowPrincipal]);
     const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
@@ -46,3 +27,4 @@ const main = async () => {
 };
 
 main();
+
