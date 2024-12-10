6b9a51b2aa5114e4616642e5028191353bebf5aa
Rafaelvillalpando21 <rafaelvillalpando21@gmail.com>
2024-12-10 08:11:04 -0700

    Usar puerto dinámico proporcionado por Railway

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 // Configurar puerto
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
